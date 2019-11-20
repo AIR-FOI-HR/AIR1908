289b05070cd0335892aa55b4e505f23c2a6297af
augarkovic <alen.ugarkovic13497@gmail.com>
2019-11-20 22:58:40 +0100

    Web service response, web service interface

---
+++ DiscountLocator19/webservice/MyWebService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace webservice
+{
+    public interface MyWebService
+    {
+        [Post("stores.php")]
+        Task<MyWebServiceResponse> getStores([Body(BodySerializationMethod.UrlEncoded)][AliasAs("method")] String method);
+
+        [Post("discounts.php")]
+        Task<MyWebServiceResponse> getDiscounts([Body(BodySerializationMethod.UrlEncoded)][AliasAs("method")] String method);
+    }
+}
---
+++ DiscountLocator19/webservice/MyWebServiceResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webservice
+{
+    public class MyWebServiceResponse
+    {
+        public int responseId { get; set; }
+        public String responseText { get; set; }
+        public long timeStamp { get; set; }
+        public String items { get; set; }
+    }
+}
